4b3411b95920088d25c425af97a63e65eca25bbd
zatserkl <zatserkl@gmail.com>
2018-08-05 01:23:30 -0700

    works for every N, debug

--- pCTreco.py
+++ pCTreco.py
@@ -6,6 +6,8 @@ Working example of Machine Learning approach to pCT image reconstruction.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator  # to set integer axis labels
+
 
 # % matplotlib inline
 
@@ -26,7 +28,8 @@ class Track:
         self.voxels = voxels[:]  # use list copy
 
     def __repr__(self):
-        s = 'Edet ' + str(Edet) + ' voxels'
+        # s = 'Edet ' + str(self.Edet) + ' voxels'
+        s = 'Edet {:6.1f} voxels:'.format(self.Edet)
         for voxel in self.voxels:
             s += ' ' + str(voxel)
         return s
@@ -42,8 +45,25 @@ class Track:
             row, col = voxel
             dE -= Track.phantom_w[row, col]
         return dE
-    
-    def loss_der(self):
+
+    # def loss_der(self):
+    #     """
+    #     --> Mainstream function
+    #     Calculates the loss function and modifies derivatives
+    #     :return: loss function
+    #     NB: modifies phantom_d passed by references
+    #     """
+    #     dE = self.Einc - self.Edet
+    #     for voxel in self.voxels:
+    #         row, col = voxel
+    #         dE -= Track.phantom_w[row, col]
+    #
+    #     # modify derivative for this voxel
+    #     for voxel in self.voxels:
+    #         Track.phantom_d[row, col] = -dE  # minus because loss = (y - h(w))
+    #     return dE
+
+    def loss_der_y_minus_h(self):
         """
         --> Mainstream function
         Calculates the loss function and modifies derivatives
@@ -58,6 +78,48 @@ class Track:
         # modify derivative for this voxel
         for voxel in self.voxels:
             Track.phantom_d[row, col] = -dE  # minus because loss = (y - h(w))
+        return dE
+
+    def loss_der_h_minus_y_doesnotwork(self):  # TODO
+        """
+        --> Mainstream function
+        Calculates the loss function and modifies derivatives
+        :return: loss function
+        NB: modifies phantom_d passed by references
+        """
+        # dE = self.Einc - self.Edet
+        Eloss = 0
+        for voxel in self.voxels:
+            row, col = voxel
+            Eloss += Track.phantom_w[row, col]
+
+        dE = Eloss - (self.Einc - self.Edet)  # h - y
+
+        # modify derivative for this voxel
+        for voxel in self.voxels:
+            Track.phantom_d[row, col] -= dE
+        return dE
+
+    def loss_der(self, phantom_d):
+        """
+        --> Mainstream function
+        Calculates the loss function and modifies derivatives
+        :return: loss function
+        NB: modifies phantom_d passed by references
+        """
+        Eloss = 0
+        for voxel in self.voxels:
+            row, col = voxel
+            # Eloss += Track.phantom_w[row, col]
+            Eloss += phantom_w[row, col]
+
+        dE = Eloss - (self.Einc - self.Edet)  # h - y
+
+        # modify derivative for all voxels of this track
+        for voxel in self.voxels:
+            row, col = voxel
+            # Track.phantom_d[row, col] += dE
+            phantom_d[row, col] += dE
         return dE
 
     def print(self):
@@ -142,7 +204,7 @@ def print_phantom(phantom):
         print()
 
 
-def costFunction_calculate(tracks, phantom_w):
+def costFunction_calculate(tracks):
     """
     Calculates cost function, does not compute derivatives
     """
@@ -155,26 +217,39 @@ def costFunction_calculate(tracks, phantom_w):
     return J / len(tracks) / 2
 
 
-def costFunction(tracks, phantom_w, phantom_d):
+def costFunction(tracks, phantom_d, alpha):
     """
     Returns cost function.
     The derivatives phantom_d will be changed in-place as passed by reference
     """
     J = 0.  # cost function
+
     # clear derivatives
-    phantom_d = np.zeros((phantom_d.shape[0], phantom_d.shape[1]))
+    # phantom_d = np.zeros((phantom_d.shape[0], phantom_d.shape[1]))
+    for row in range(phantom_d.shape[0]):
+        for col in range(phantom_d.shape[1]):
+            phantom_d[row, col] = 0
 
     for track in tracks:
-        loss = track.loss_der()  # calcs loss function J and modifies phantom_d
+        loss = track.loss_der(phantom_d)  # calcs loss function J and modifies phantom_d
         J += loss * loss
 
-    phantom_d /= len(tracks)
+    print('\n"    derivatives" phantom - before norm\n')
+    print_phantom(phantom_d)
+
+    phantom_d *= alpha / len(tracks)  # alpha is a learning rate
+
+    print('\n"    derivatives" phantom - after norm alpha =',
+          alpha, 'len(tracks) =', len(tracks), '\n')
+    print_phantom(phantom_d)
+
     return J / len(tracks) / 2
 
 
 if __name__ == '__main__':
 
-    N = 2
+    N = 10
+    # for N = 4 and phantom_v[1, 1] = 10 there is 2-point raise -- TODO
     phantom_v = np.zeros((N, N))  # true values
     phantom_w = np.zeros((N, N))  # weights
     phantom_d = np.zeros((N, N))  # derivatives
@@ -192,12 +267,13 @@ if __name__ == '__main__':
     #
     # set the phantom density (in terms of energy loss)
     #
-    phantom_v[1, 1] = 10  # loss 10 MeV
-    phantom_v[1, 0] = 3   # loss 3 MeV
-    phantom_v[0, 0] = 5   # loss 5 MeV
 
-    # print('"real" phantom\n')
-    # print_phantom(phantom_v)
+    # phantom_v[0, 0] = 10.  # loss 10 MeV  # seems to be fine for all
+    # phantom_v[1, 1] = 10.
+
+    phantom_v[1, 1] = 10.  # loss 10 MeV
+    phantom_v[1, 0] = 3.   # loss 3 MeV
+    phantom_v[0, 0] = 5.   # loss 5 MeV
 
     # Generate rays and create tracks
     tracks = []
@@ -214,23 +290,25 @@ if __name__ == '__main__':
             Elost += phantom_v[row, col]
         track.Edet = Einc - Elost
 
-    print('\nMeasured tracks:')
+    print('\nMeasured tracks ({} tracks):'.format(len(tracks)))
 
-    for track in tracks:
-        track.print()
+    for i, track in enumerate(tracks):
+        print(i, '\t', track)
 
 
     ###############################################
 
+    # alpha = 0.1  # learning rate
+    alpha = 1.  # learning rate
 
     cost = []
-    cost.append(costFunction_calculate(tracks, phantom_w))
+    cost.append(costFunction_calculate(tracks))
 
-    iter_max = 3
+    iter_max = 100
     for iter in range(1, iter_max+1):
         print('\n--------------- iteration', iter, '---------------')
 
-        Ji = costFunction(tracks, phantom_w, phantom_d)
+        Ji = costFunction(tracks, phantom_d, alpha)
         print('\nInitial Cost function Ji =', Ji)
 
         print('\n"real" phantom\n')
@@ -248,11 +326,15 @@ if __name__ == '__main__':
         print('\n"weights" phantom after correction\n')
         print_phantom(phantom_w)
 
-        Jf = costFunction_calculate(tracks, phantom_w)
+        Jf = costFunction_calculate(tracks)
         print('\nFinal Cost function Jf =', Jf)
         cost.append(Jf)
 
     plt.figure()
-    plt.plot(cost, marker='o', linestyle='None')
+    plt.plot(cost, marker='.', linestyle='None')
     plt.grid()
+    plt.gcf().gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.xlabel('iteration')
+    plt.tight_layout()
+    plt.title('N = {}'.format(N))
     plt.show()
